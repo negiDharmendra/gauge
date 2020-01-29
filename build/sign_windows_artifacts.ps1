e68322f6df03300ad0056bf2f33126aea76b392d
Dharmendra Singh <dharmenn@thoughtworks.com>
2020-01-29 17:39:14 +0530

    debug

--- build/sign_windows_artifacts.ps1
+++ build/sign_windows_artifacts.ps1
@@ -34,5 +34,14 @@ if ($LastExitCode -ne 0) {
 Pop-Location
 
 $nightlyFlag = If ($nightly) {"--nightly"} Else {""}
-& go run build/make.go --distro --certFile $env:CERT_FILE --bin-dir bin\windows_amd64 $nightlyFlag
-& go run build/make.go --distro --certFile $env:CERT_FILE --bin-dir bin\windows_386 $nightlyFlag
+& go run build/make.go --distro  --bin-dir bin\windows_amd64 $nightlyFlag
+& go run build/make.go --distro  --bin-dir bin\windows_386 $nightlyFlag
+
+Get-ChildItem "$pwd\deploy" |
+ForEach-Object {
+    $fileName  = "$pwd\deploy\$_"
+    signtool sign /debug /v /tr http://timestamp.digicert.com /a /fd sha256 /td sha256 /f $env:CERT_FILE /as $fileName
+    if ($LastExitCode -ne 0) {
+         throw "$fileName signing failed"
+    }
+}
